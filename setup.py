746356f2eb1fc7035216477107d01759550e70c6
Maxime Cognie <80749639+maximecognie@users.noreply.github.com>
2022-02-23 11:00:59 +0100

    modifying the package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
 
-d['packages'] = ['geotagging']
+d['packages'] = ['img_geotagger']
 d['package_dir'] = {'' : 'src'}
 
 setup(**d)
