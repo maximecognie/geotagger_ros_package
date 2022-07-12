1ceac7f61074f5a279cf900fcf5ac32dc8c56567
mcognie <80749639+mcognie@users.noreply.github.com>
2022-07-12 12:24:49 +0200

    Update geotagger_node.py

--- src/geotagging/geotagger_node.py
+++ src/geotagging/geotagger_node.py
@@ -102,7 +102,7 @@ class mapping_hexa:
     rospy.loginfo("gps time: %i", gps_data.header.stamp.secs)
 
     try:
-      cv_img = self.bridge.imgmsg_to_cv2(img_data, "bgr8")
+      cv_img = self.bridge.imgmsg_to_cv2(img_data, desired_encoding='passthrough')
     except CvBridgeError as e:
       print(type(img_data))
       print(e)
